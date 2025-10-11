d851b18d57c18a35841ae683a06c533770a4d3e7
OmarjLand <omar.land2010@gmail.com>
2025-10-11 23:14:20 +0200

    Añadiendo mejoras al Challenge de Weather

--- 3 weather-app-main/weather-app-main/js/scripts.js
+++ 3 weather-app-main/weather-app-main/js/scripts.js
@@ -1,1 +1,12 @@
 console.log('Hola holitas');
+
+const url = `https://api.open-meteo.com/v1/forecast?latitude=43.3713&longitude=-8.396&daily=temperature_2m_max,temperature_2m_min,weather_code&hourly=temperature_2m&current=precipitation,wind_speed_10m,apparent_temperature,relative_humidity_2m,temperature_2m`
+
+fetch(url)
+    .then(resp => resp.json())
+    .then(data => {
+        const datos = data;
+        console.log(datos);
+        
+    })
+
